08d4e4135160357e36fda5b9dd71491a4004bee0
Ayub <ayubhanif06@gmail.com>
2025-05-23 20:43:36 -0700

    Added:  the structure and layout of the frontend with mockdata now just gonna pull the data. Future: I will pull the mock data and then start with the UI designing and other stuff.

---
+++ frontend/src/api_data/client.ts
@@ -0,0 +1,18 @@
+import type { Restaurant, Publish } from '../interface_data/index.ts';
+
+//mock data for now
+const BASE = '/api/v3';
+
+//not yet called but I will after the intial setup and checking the mock data
+//for now we just gonna use the mock data using the generic function
+async function fetchAPI<T>(paths: string): Promise<T> {
+
+  const response = await fetch(`${BASE}${paths}`);
+
+  //the T is the type of the data we are getting which for now is Restaurant[] or Publish[]
+  return response.json() as T;
+
+}
+
+export const getRestaurants = () => fetchAPI<Restaurant[]>('/restaurant');
+export const getPublishes  = () => fetchAPI<Publish[]>('/publish')
