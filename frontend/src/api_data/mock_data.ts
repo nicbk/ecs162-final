cff7ccd34ffa48a70d514170768ea33d442632cf
Ayub <ayubhanif06@gmail.com>
2025-05-26 17:24:18 -0700

    fixed the mock data and also made the home page navigation bar more what we want as we discussed in the meeting.

--- frontend/src/api_data/mock_data.ts
+++ frontend/src/api_data/mock_data.ts
@@ -1,64 +1,77 @@
 //expecting the api to look like this with data given from the backend
 
-import { type Restaurant, type Publish } from "../interface_data/index.ts";
+import { type Restaurant, type Comment } from "../interface_data/index.ts";
+import mock_food_mac from "../assets/MAC_Burger_mock_pic.jpg";
+import in_and_out from "../assets/inandout.jpg";
+import bonappetit from "../assets/bonappetit.png";
 
 fetch('/api/v1/restaurant')
 export const mockResturantsData: Restaurant[] = [
     {
-        id: '1',
-        name: "In-N-Out Burger",
-        Url: "https://image.com.jpg",
+        restaurantId: '1',
+        restaurantTitle: "In-N-Out Burger",
+        rating: 7.0,
         address: "234 Davis St",
-        city: "Davis",
-        state: "CA",
-        zip_code: "92342",
-        phone_number: "234-234-2342",
-        website: "www.Mockdata.com",    },
+        images: [bonappetit],
+        googleMapsUrl: "google.com/maps/In-N-Out",
+       },
     {
-        id: '2',
-        name: "MAC Burger",
-        Url: "https://image.com.jpg",
+        restaurantId: '2',
+        restaurantTitle: "MAC Burger",
+        rating: 3.0,
         address: "543 Davis St",
-        city: "Davis",
-        state: "CA",
-        zip_code: "23423",
-        phone_number: "233-2134-2412",
-        website: "www.Mockdata.com",
+        images: [mock_food_mac, "https://cdn.sanity.io/images/czqk28jt/prod_bk_us/5307fcecf8985c350677b2721122facddbf949c8-2000x1000.png?w=650&q=75&fit=max&auto=format"],
+        googleMapsUrl: "google.com/maps/MAC",
     },
     {
-        id: '3',
-        name: "Taco Bell",
-        Url: "https://image.com.jpg",
+        restaurantId: '3',
+        restaurantTitle: "Taco Bell",
+        rating: 10.0,
         address: "4444 Davis St",
-        city: "Davis",
-        state: "CA",
-        zip_code: "66666",
-        phone_number: "544-234-2542",
-        website: "www.Mockdata.com",
+        images: [in_and_out],
+        googleMapsUrl: "google.com/maps/Taco",
     },
 
 ];
-fetch('/api/v1/Publish')
-export const mockPublish: Publish [] = [
+fetch('/api/v1/Comment')
+export const mockPublish: Comment [] = [
     {
-        id:'234',
-        title: "resturant post",
-        content: "come get your food!!!",
+        id: '234',
+        username: "ayub",
+        body: "this is a comment",
+        images: ["data:image/jpeg;base64,sdifjaijew", "data:image/jpeg;base64,aifwjwjefijawef"],
+        likes: 15,
+        deleted: false,
+        replies: [
+            {
+                id: '2234341',
+                username: "john",
+                body: "I am a reply",
+                images: ["data:image/jpeg;base64,sdifjaijew", "data:image/jpeg;base64,aifwjwjefijawef"],
+                likes: 2,
+                deleted: false,
+                replies: []
+            }
+        ]
     },
-    {
-        id:'235',
-        title: "resturant post#2",
-        content: "I Love pizza!!!",
-    },
-    {
-        id:'236',
-        title: "resturant post#3",
-        content: "new opening!!!",
-    },
-    {
-        id:'237',
-        title: "resturant post#4",
-        content: "IDK yet!!!",
+        {
+        id: '222222',
+        username: "sam",
+        body: "food was good",
+        images: ["data:image/jpeg;base64,jjfgtr", "data:image/jpeg;base64,hgffg"],
+        likes: 1,
+        deleted: false,
+        replies: [
+            {
+                id: '2234341',
+                username: "john",
+                body: "lies this is a lie, it was bad",
+                images: ["data:image/jpeg;base64,drtfdghfghd", "data:image/jpeg;base64,fghhgf"],
+                likes: 13,
+                deleted: false,
+                replies: []
+            }
+        ]
     },
 
 ];
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -7,16 +7,15 @@ export default function Home() {
   return (
       <div className={styles.home}>
         {mockResturantsData.map((rest: Restaurant) => (
-          <div className={styles.card} key={rest.id}>
+          <div className={styles.card} key={rest.restaurantId}>
             <div className={styles.post}>
-              <h4>{rest.name}</h4>
+              <h4>{rest.restaurantTitle}</h4>
               <p>{rest.address}</p>
-              <p>{rest.city}</p>
-              <p>{rest.state}</p>
+              <p>{rest.rating}</p>
             </div>
             
             <div className={styles.cardImage}>
-              <img src="https://assets.bonappetit.com/photos/631788f25635b01b337f6bb4/16:9/w_2560%2Cc_limit/220827_GuangXu_BA-UncleLou_014.jpg"
+              <img src={rest.images[0]}
                alt="Card Image" />
             </div>
             <div className={styles.cardHeader}>
