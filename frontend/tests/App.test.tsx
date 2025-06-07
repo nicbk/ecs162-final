6bc15d70234686265a8561289699b180621478c6
Andrew Ortega <ortegaandrew8@gmail.com>
2025-06-07 15:35:31 -0700

    Added more testing for profile and home page

--- frontend/tests/App.test.tsx
+++ frontend/tests/App.test.tsx
@@ -42,6 +42,123 @@ test('Opening and closing the sidebar', async () => {
   expect(closeButton).toBeVisible();
   await closeButton.click();
 
-  //foodie text should no longer be on screen
-  await expect.element(page.getByText("Foodie")).not.toBeVisible();
+  //Should be on home screen with a like button
+  const likeButton = screen.getByLabelText("Like").first();
+  expect(likeButton).toBeVisible()
+})
+
+test('Go to profile page and back to home', async () => {
+  //On Home page
+  const screen = page.render(
+    <MemoryRouter initialEntries={['/Home']}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </MemoryRouter>
+  )
+  //Open the menu
+  const openButton = screen.getByLabelText("Open menu");
+  expect(openButton).toBeVisible();
+  await openButton.click()
+
+  //Go to profile
+  const profileButton = screen.getByText('Profile');
+  await profileButton.click();
+
+  //Verify we are at profile page
+  const commentSection = screen.getByText("Your comments");
+  expect(commentSection).toBeVisible();
+
+  //Go back to home page
+  const homeButton = screen.getByText('Home');
+  await homeButton.click();
+
+  const likeButton = screen.getByLabelText("Like").first();
+  expect(likeButton).toBeVisible()
+})
+
+test('Go to profile page and editing bio', async () => {
+  //On Home page
+  const screen = page.render(
+    <MemoryRouter initialEntries={['/Home']}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </MemoryRouter>
+  )
+  //Open the menu
+  const openButton = screen.getByLabelText("Open menu");
+  expect(openButton).toBeVisible();
+  await openButton.click()
+
+  //Go to profile
+  const profileButton = screen.getByText('Profile');
+  await profileButton.click();
+
+  //Close menu
+  const closeButton = screen.getByLabelText("Close menu");
+  await closeButton.click();
+
+  //See if original bio is there (test bio)
+  const originalBio = screen.getByText("test bio");
+  expect(originalBio).toBeVisible();
+
+  //Edit profile
+  const editProfileButton = screen.getByText('Edit Profile');
+  await editProfileButton.click();
+
+  //Get the bio area since can also edit username currently
+  const bioTextarea = screen.getByRole('textbox').nth(1);
+  await bioTextarea.fill("new bio");
+
+  // Save the changes
+  const saveButton = screen.getByText('Save');
+  await saveButton.click();
+
+  // Verify the new bio is displayed
+  const newBio = screen.getByText("new bio");
+  expect(newBio).toBeVisible();
+})
+
+test('Go to profile page and cancel bio edit', async () => {
+  //On Home page
+  const screen = page.render(
+    <MemoryRouter initialEntries={['/Home']}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </MemoryRouter>
+  )
+  //Open the menu
+  const openButton = screen.getByLabelText("Open menu");
+  expect(openButton).toBeVisible();
+  await openButton.click()
+
+  //Go to profile
+  const profileButton = screen.getByText('Profile');
+  await profileButton.click();
+
+  //Close menu
+  const closeButton = screen.getByLabelText("Close menu");
+  await closeButton.click();
+
+  //See if original bio is there (test bio)
+  const originalBio = screen.getByText("test bio");
+  expect(originalBio).toBeVisible();
+
+  //Edit profile
+  const editProfileButton = screen.getByText('Edit Profile');
+  await editProfileButton.click();
+
+  //Get the bio area since can also edit username currently
+  const bioTextarea = screen.getByRole('textbox').nth(1);
+  await bioTextarea.fill("new bio");
+
+  // cancel the changes
+  const cancelButton = screen.getByText('Cancel');
+  await cancelButton.click();
+
+  // Verify the original bio is still there
+  const originalBio2 = screen.getByText("test bio");
+  expect(originalBio2).toBeVisible();
 })
